22012fb3c9c415c9eb30d0cb056410e5243ac529
chandra-react-course <chandra.reactcourse@gmail.com>
2024-10-02 12:47:16 +0530

    Episode-6

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import Header from "./components/Header"
-import Body from "./components/Body"
-import Footer from "./components/Footer"
+import Header from "./components/Header";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
 
 
 const FoodDelivery = () => {
--- src/components/Body.js
+++ src/components/Body.js
@@ -9,32 +9,42 @@ const Body = () => {
    const [filterdList, setFilterdList] = useState([]);
    const [searchInput, setSearchInput] = useState("");
 
-   useEffect(() => {
-    fetchData();
-   })
 
-const fetchData = async () =>{
+useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
     const data = await fetch(
-        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.5096133&lng=78.3168628&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.5096133&lng=78.3168628&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
+
     const json = await data.json();
 
-    setListOfRes(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
-    setFilterdList(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants);
-}
+    // Optional Chaining
+    setListOfRes(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  
+    setFilterdList(
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+   
+  };
+
   
     return listOfRes.length === 0 ? <Shimmer/> : (
         <div className="body-main">
             <div className="search">
                 <input type="text" className="search-input" value={searchInput} onChange={(e) =>{
                     setSearchInput(e.target.value);
-                }}></input>
+                }}/>
                 <button className="input-btn" onClick={()=>{
-                   const searchfilterList = listOfRes.filter(res=>res.info.name.toLowerCase().includes(searchInput));
+                   const searchfilterList = listOfRes.filter((res)=>res.info.name.toLowerCase().includes(searchInput));
                    setFilterdList(searchfilterList);
                 }}>Search</button>
                 <button className="rated-btn" onClick={() =>{
-                    const avgFiltered = listOfRes.filter(res => res.info.avgRating > 4.2);
+                    const avgFiltered = listOfRes.filter((res) => res.info.avgRating > 4.2);
                     setFilterdList(avgFiltered);
                 }}>Top Rated Restuarant</button>
             </div>
