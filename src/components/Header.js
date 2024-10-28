86d1c097e56d09ce3436e6ef497206bc9cb27f62
chandrasekhar <chandra.reactcourse@gmail.com>
2024-10-28 20:53:22 +0530

    Episode-12

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,13 @@ import {APP_LOGO} from "../constants/const";
 import { Link } from "react-router-dom";
 import useOnlineStatus from "../utils/useOnlineStatus";
 import UserContext from "../utils/userContext";
+import { useSelector } from "react-redux";
 
 const Header = () =>{
    const [loginBtn, setLoginBtn] = useState("Login");
    const onlineStatus = useOnlineStatus();
    const userContext = useContext(UserContext);
+   const cartItems = useSelector((store)=>store.cart.items);
 
    useEffect(() => {
     }, [loginBtn]);
@@ -26,7 +28,7 @@ const Header = () =>{
                     <li className="px-4"><Link to={"/about"}>About</Link></li>
                     <li className="px-4"><Link to={"/contact"}>Contact</Link></li>
                     <li className="px-4"><Link to={"/grocery"}>Grocery</Link></li>
-                    <li className="px-4">Cart</li>
+                    <li className="px-4"><Link to={"/cart"}>Cart ({cartItems.length} Items)</Link></li>
                     <button className="px-4" onClick={() =>{
                       loginBtn === "Login" ? setLoginBtn("Logout") :setLoginBtn("Login");
                     }}>{loginBtn}</button>
